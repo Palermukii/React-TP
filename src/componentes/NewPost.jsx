5a6a4e5491dd32441c6b9e4bae0370ebb2b8d87a
Paler <tomasp2266@gmail.com>
2023-10-05 11:41:01 -0300

    Pusimos ID, Barra de busqueda

--- src/componentes/NewPost.jsx
+++ src/componentes/NewPost.jsx
@@ -9,9 +9,14 @@ const [tittle, setTittle] = useState("");
 const [article, setArticle] = useState("");
 const [post, setPost] = useState([]);
 
+
+useEffect(()=> {
+  let posteos = JSON.parse(localStorage.getItem('post'));
+  if (posteos) setPost(posteos);
+},[])
 const handleSubmit = (e) => {
     e.preventDefault();
-    const newPosts = [...post, { name, tittle, article }];
+    const newPosts = [{ name, tittle, article, id: post.length }, ...post];
     setPost(newPosts);
     console.log(newPosts);
     localStorage.setItem('post', JSON.stringify(newPosts));
@@ -22,37 +27,40 @@ const handleSubmit = (e) => {
 
 
   return (
-      <div className='NewPost'>
-        <div className='Header'>
-        <button><a href="/">Home</a></button>
-          <button><a href="/newpost">New Post</a></button>
-        </div>
-        <div>
-        <h1>¡Crea tu propio Post!</h1>
-      <form onSubmit={handleSubmit} className="Form">
-        <input className='input' 
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Nombre"
-        />
-        <input className='input'
-          type="text"
-          value={tittle}
-          id="titulo"
-          onChange={(e) => setTittle(e.target.value)}
-
-          placeholder="Título"
-        />
-        <textarea value={article} id="Articulo" cols="40" rows="10" onChange={(e) => setArticle(e.target.value)}></textarea>
-        <button type='submit'>Publicar</button>
-      </form>
-      {post && <Posts post={post} />} 
-
-      
-    </div>
+    <div className='NewPost'>
+      <div className='Header'>
+      <button className='botonHeader'><a href="/">Home</a></button>
+        <button className='botonHeader'><a href="/newpost">New Post</a></button>
       </div>
-  );
+      <div>
+      <h1>¡Crea tu propio Post!</h1>
+    <form onSubmit={handleSubmit} className="Form">
+      <input className='input' 
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Nombre"
+      />
+      <input className='input'
+        type="text"
+        value={tittle}
+        id="titulo"
+        onChange={(e) => setTittle(e.target.value)}
+
+        placeholder="Título"
+      />
+      <textarea value={article} id="Articulo" cols="40" rows="10" onChange={(e) => setArticle(e.target.value)}></textarea>
+      <button className="publicar" type='submit'>Publicar</button>
+    </form>
+    {post && <Posts post={post} />} 
+
+    
+  </div>
+    </div>
+);
 };
 
 export default NewPost;
+
+
+
